081fed2c4f34c5cca99b8154e942cc0a91262339
han032206 <han032206@gmail.com>
2023-07-04 01:36:48 +0800

    test api

--- pilot/server/knowinitserver.py
+++ pilot/server/knowinitserver.py
@@ -1,5 +1,18 @@
 from flask import Flask, request
+from pilot.configs.config import Config
 import os
+from pilot.configs.model_config import (
+    DATASETS_DIR,
+    KNOWLEDGE_UPLOAD_ROOT_PATH,
+    LOGDIR,
+    LLM_MODEL_CONFIG,
+)
+import shutil
+from pilot.source_embedding.knowledge_embedding import KnowledgeEmbedding
+vector_store_name = {"vs_name": ""}
+CFG = Config()
+from pilot.utils import build_logger
+logger = build_logger("webserver", LOGDIR + "webserver.log")
 
 app = Flask(__name__)
 
@@ -25,27 +38,40 @@ def knowledge_embedding_store(vs_id, files):
 
     logger.info("knowledge embedding success")
     return vs_id
+
+def save_vs_name(vs_name):
+    vector_store_name["vs_name"] = vs_name
+    return vs_name
+
+
 # 定义接口路由
 @app.route('/api/embedding', methods=['POST'])
 def embedding():
     # 获取请求参数
     group = request.form.get('group')
-    data_path = request.form.get('data_path')
-
-    # 调用 knowledge embedding 函数进行处理
-    embedding_data = knowledge_embedding_store(group, data_path)
+    file = request.files['file']
 
     # 按用户组名字命名数据库
     db_name = 'embedding_' + group
 
-    # 将处理后的数据插入数据库
-    
+    if not os.path.exists(os.path.join(DATASETS_DIR, db_name)):
+        os.makedirs(os.path.join(DATASETS_DIR, db_name))
+    # save the file to the directory
+    file.save(os.path.join(DATASETS_DIR, db_name, file.filename))
 
-    # 设置数据库为待检索状态
-    
+    files = []
+    for dirpath, dirnames, filenames in os.walk(os.path.join(DATASETS_DIR, db_name)):
+        files.append(os.path.join(dirpath,dirnames,filenames))
+
+
+    db_name = save_vs_name(db_name)
+    # 调用 knowledge embedding 函数进行处理
+    embedding_data = knowledge_embedding_store(db_name, files)
+
+    # 将处理后的数据插入数据库
 
     # 返回处理后的数据
-    return 'OK'
+    return 'knowledge embedding success'
 
 if __name__ == '__main__':
     app.run(debug=True)
