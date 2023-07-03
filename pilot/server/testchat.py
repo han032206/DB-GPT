b81e7baef72b73bfde139c33abd742b2cb99f218
han032206 <han032206@gmail.com>
2023-07-04 05:36:30 +0800

    add chat api

--- pilot/server/testchat.py
+++ pilot/server/testchat.py
@@ -1,20 +1,35 @@
+import os
+import tempfile
 import unittest
-from app import app
+import requests
+import pandas as pd
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
-        app.testing = True
-        self.client = app.test_client()
+        self.app_url = 'http://127.0.0.1:5060/api/generate'
+        self.test_message = '你好！'
+        self.test_group = 'test_group'
 
-    def test_generate(self):
-        # Send a POST request to the API endpoint with a test message and group
-        data = {'group': 'test_group', 'message': '你好！'}
-        response = self.client.post('/api/generate', data=data)
+    def tearDown(self):
+        os.remove(self.test_file_path)
+
+    def test_embedding(self):
+        # # Create a temporary test file
+        # df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']})
+        # df.to_excel(self.test_file_path, index=False)
+
+        # Send a POST request to the API endpoint
+    # with open(self.test_file_path, 'rb') as f:
+    #     files = {'file': f}
+    #     data = {'group': self.test_group}
+        message = {'message':self.test_message}
+        group = {'group':self.test_group}
+        response = requests.post(self.app_url, group=group, message=message)
 
         # Check the response status code
         self.assertEqual(response.status_code, 200)
 
-        # Check that the response contains generated content
+        # Check that the knowledge embedding was successful
         self.assertGreater(len(response.data), 0)
 
 if __name__ == '__main__':
