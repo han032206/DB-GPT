9a4ffda79a3be61b422983e2308854973153dac6
han032206 <han032206@gmail.com>
2023-07-04 04:52:37 +0800

    debug

--- pilot/server/deleteserver.py
+++ pilot/server/deleteserver.py
@@ -91,4 +91,4 @@ def embedding():
     return 'knowledge delete and embedding success'
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5050)
+    app.run(debug=True,host='0.0.0.0',port=5050)
